945f0196ffae19794c68c710e7d6156029d440dd
masafm <masafumi.kashiwagi@datadoghq.com>
2023-07-14 14:11:50 +0900

    update

--- python/app.py
+++ python/app.py
@@ -6,6 +6,8 @@ app = Flask(__name__)
 
 @app.route("/",methods=['post','get'])
 def home():
-    return Response(request.get_data(), status=200, mimetype='application/json')
+    text=request.get_data();
+    print(text, flush=True)
+    return Response(text, status=200, mimetype='application/json')
 
 app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
