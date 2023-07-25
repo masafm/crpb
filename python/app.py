78e34ebef7ca19847ee3fd335157180d15d3dfb2
masafm <masafumi.kashiwagi@datadoghq.com>
2023-07-25 19:54:12 +0900

    update

--- python/app.py
+++ python/app.py
@@ -1,13 +1,32 @@
-import os
-from flask import Flask, Response
-from flask import request
+import socket
 
-app = Flask(__name__)
+server_ip = "0.0.0.0"
+server_port = 8080
+listen_num = 5
+buffer_size = 1024
 
-@app.route("/",methods=['post','get'])
-def home():
-    text=request.get_data();
-    print(text, flush=True)
-    return Response(text, status=200, mimetype='application/json')
+# 1.ソケットオブジェクトの作成
+tcp_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+# 2.作成したソケットオブジェクトにIPアドレスとポートを紐づける
+tcp_server.bind((server_ip, server_port))
+
+# 3.作成したオブジェクトを接続可能状態にする
+tcp_server.listen(listen_num)
+
+# 4.ループして接続を待ち続ける
+while True:
+    # 5.クライアントと接続する
+    client,address = tcp_server.accept()
+    print("[*] Connected!! [ Source : {}]".format(address))
+
+    # 6.データを受信する
+    data = client.recv(buffer_size)
+    print("[*] Received Data : {}".format(data))
+
+    # 7.クライアントへデータを返す
+    client.send(b"ACK!!")
+
+    # 8.接続を終了させる
+    client.close()
+
