fe565958d31e5191e1ab06c65cf9aea3e2ad40ae
masafm <masafumi.kashiwagi@datadoghq.com>
2023-08-01 10:16:40 +0900

    update

--- python/app.py
+++ python/app.py
@@ -1,4 +1,5 @@
 import os
+import re
 from flask import Flask, Response
 from flask import request
 
@@ -6,9 +7,8 @@ app = Flask(__name__)
 
 @app.route("/",methods=['post','get'])
 def home():
-    text=request.get_data();
-    #print(text, flush=True)
-    print(str(request.headers)+text.decode('utf-8'), flush=True)
+    text=str(request.headers)+request.get_data().decode('utf-8')
+    print(re.sub('\r?\n', ',,', text), flush=True)
     return Response(text, status=200, mimetype='application/json')
 
 app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
